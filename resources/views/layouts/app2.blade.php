41ba8239fba8d4e427ca7cc90746b5dd5ada4c90
Alemhulu <alemhulu@gmail.com>
2021-03-04 17:47:56 +0300

    single search fixed

--- resources/views/layouts/app2.blade.php
+++ resources/views/layouts/app2.blade.php
@@ -3,8 +3,8 @@
 
 <head>
     <meta charset="utf-8">
-     <meta name="MoE" content="Ministry of Education">
-  <meta name="author" content="Alemhulu AWekelgne">
+    <meta name="MoE" content="Ministry of Education">
+    <meta name="author" content="Alemhulu AWekelgne">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>MoE</title>
     <meta name="csrf-token" content="{{ csrf_token() }}" />
@@ -26,3 +26,4 @@
    
 </body>
 </html>
+ 
--- resources/views/layouts/app3.blade.php
+++ resources/views/layouts/app3.blade.php
@@ -3,8 +3,8 @@
 
 <head>
     <meta charset="utf-8">
-     <meta name="MoE" content="Ministry of Education">
-  <meta name="author" content="Alemhulu AWekelgne">
+    <meta name="MoE" content="Ministry of Education">
+    <meta name="author" content="Alemhulu AWekelgne">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>MoE</title>
@@ -18,16 +18,10 @@
 <body style="overflow-x:hidden;" id="body" >
     
        @include('inc.nav')
-       
-        
-        
-
-        <div>
-            @include('inc.messages')
-            @yield('content')
+       @include('inc.messages')
+       @yield('content')
             
-        </div>
-        
+      
     </div>
    
     
