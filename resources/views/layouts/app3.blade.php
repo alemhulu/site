924a751f47e3104dc9cbac0e6f6843db43b14e22
Alemhulu <alemhulu@gmail.com>
2021-04-09 16:28:49 +0300

    title changed ;

--- resources/views/layouts/app3.blade.php
+++ resources/views/layouts/app3.blade.php
@@ -9,7 +9,7 @@
     <meta name="google-site-verification" content="YA8sQHO3FyF0B5Ozb0Y96IbN3kgetyaR-fOIN-fa3dk" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-     <title>MoE E-Learning and Digital Library</title>
+     <title>Ministry of Education Ethiopia</title>
     <link rel="stylesheet" href="/assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
     <link rel="stylesheet" href="/assets/fonts/fontawesome-all.min.css">
