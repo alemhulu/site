e9d7d4d86681562cdad8a4d1f38f92735c798e6b
Alemhulu <alemhulu@gmail.com>
2021-05-28 17:41:03 +0300

    https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,7 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Pagination\Paginator;
-use \ILLuminate\Support\Facades\URL::forceScheme('https');
+use ILLuminate\Support\Facades\URL::forceScheme('https');
 
 class AppServiceProvider extends ServiceProvider
 {
