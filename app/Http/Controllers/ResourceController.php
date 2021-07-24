c0eee55a36fceb332bdba997df2aff190aebb4be
alemhulu <alemhulu@gmail.com>
2021-07-24 21:50:49 +0300

    single file upload on work

--- app/Http/Controllers/ResourceController.php
+++ app/Http/Controllers/ResourceController.php
@@ -33,115 +33,33 @@ class ResourceController extends Controller
     return array_merge($tree, $branch);
     }
 
+    //upload function for from file and folder order
     public function upload($request){
         $resource = new Resource;
-    //if(request('unit_id')==0&&request('description')=='')
-    //{
-    //return ('Error!!');
-    //}
-    $user_id            = Auth::user()->id;
-    $resource->fileLocation  = request('fileLocation');
-    $resource->thumbnailLocation = request('thumbnailLocation');
-    $resource->link = request('link');
-    if(request('grade_id')!=0)
-    $resource ->grade_id = request('grade_id');
-    if(request('course_id')!=0)
-    $resource ->course_id = request('course_id');
-    if(request('unit_id')!=0)
-    $resource ->unit_id = request('unit_id');
-    if(request('subunit_id')!=0)
-    $resource ->subunit_id = request('subunit_id');
-    if(request('type_id')!=0)
-    $resource ->type_id = request('type_id');
-    if(request('media_id')!=0)
-    $resource ->media_id = request('media_id');
+        $user_id            = Auth::user()->id;
+        $resource->fileLocation  = request('fileLocation');
+        $resource->thumbnailLocation = request('thumbnailLocation');
+        $resource->link = request('link');
+        if(request('grade_id')!=0)
+        $resource ->grade_id = request('grade_id');
+        if(request('course_id')!=0)
+        $resource ->course_id = request('course_id');
+        if(request('unit_id')!=0)
+        $resource ->unit_id = request('unit_id');
+        if(request('subunit_id')!=0)
+        $resource ->subunit_id = request('subunit_id');
+        if(request('type_id')!=0)
+        $resource ->type_id = request('type_id');
+        if(request('media_id')!=0)
+        $resource ->media_id = request('media_id');
 
-    $resource->user_id      = $user_id;
-    $resource ->description = request('description');
-    $resource ->tag = request('tag');
-    $resource -> save();
-
+        $resource->user_id      = $user_id;
+        $resource ->description = request('description');
+        $resource ->tag = request('tag');
+        $resource -> save();
     }
 
-    public function great(Request $request){
-
-         //real path accessing the destination folder
-                        $path = storage_path().'/app/public/biology/video/plasma';
-                        //static value for storage in database;
-                        $pathname = '/storage/biology/video/plasma';
-
-                        $tree = $this->getTree($path);
-                        
-                        $x=0;
-
-                        foreach ($tree['children'] as $alex){
-                            $x++;
-                        }
-                        return $x;
-                        $list_of_files = [];
-                      
-                        foreach($tree["children"] as $child)
-                        {
-                                $child_path = $pathname.$child;
-                                
-                                $name = substr($child,0, -4);
-                                // return public_path();
-                                // return $child_path.'      -----  '.$name ;
-
-                                //storing
-                             $course_id= ' ';
-                             $grade_id= ' ';
-                             $link= ' ';
-                             $media_id= ' ';
-                             $tag= ' ';
-                             $thumbnailLocation= ' ';
-                             $type_id = ' ';
-                             $unit_id = ' ';
-
-                             $description= $name;
-                             $fileLocation= $child_path;
-                             
-                             
-                             $resource = new Resource;
-                             $user_id= Auth::user()->id;
-                             $resource->fileLocation  = $fileLocation;
-                             $resource->thumbnailLocation = $thumbnailLocation;
-                             $resource->link = $link;
-                            //  if(request('grade_id')!=0)
-                             $resource ->grade_id = $grade_id;
-                            //  if(request('course_id')!=0)
-                             $resource ->course_id = $course_id;
-                            //  if(request('unit_id')!=0)
-                             $resource ->unit_id = $unit_id;
-                            //  if(request('subunit_id')!=0)
-                            //  $resource ->subunit_id = request('subunit_id');
-                            //  if(request('type_id')!=0)
-                             $resource ->type_id = $type_id;
-                            //  if(request('media_id')!=0)
-                             $resource ->media_id = $media_id;
-                     
-                             $resource->user_id= $user_id;
-                             $resource ->description = $description;
-                             $resource ->tag = $tag;
-                             return $resource;
-                             $resource -> save();  
-                 
-
-
-                                // $this->validate($request,[
-                                //     'course_id'=> 'required',
-                                //         'fileLocation'=> 'required',
-                                //         'thumbnailLocation'=> 'required',
-                                //         'link'=> 'required',
-                                //         'media_id'=> 'required',
-                                //         'type_id'=> 'required',
-                                //         'tag'=> 'required',
-                                //         'description'=> 'required',
-                                //     ]);
-                            
-                             
-                        }
-                }
+ 
     /**
      * Display a listing of the resource.
      *
@@ -184,6 +102,7 @@ class ResourceController extends Controller
                     'tag'=> 'required',
                     'description'=> 'required',
                 ]);
+                $this->upload($request);
         }
         else if($request->fileLocation=="" & $request->folderLocation!=""){
             $this->validate($request,[
@@ -237,10 +156,6 @@ class ResourceController extends Controller
             abort(403, 'Please choose file or folder upload!');
         }
 
-        
-       
-
-        
     }
 
     /**
