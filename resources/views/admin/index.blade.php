3beba705abf7ba94c55bc43d61f82579a0ebee38
alemhulu <alemhulu@gmail.com>
2022-01-13 09:22:48 +0300

    report

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -11,8 +11,8 @@
     <li class="breadcrumb-item active">Overview</li>
   </ol>
   <div class="row ">
-    <div class="col-6">@livewire('monthly-views-chart')</div>
-    <div class="col-6 mb-4">@livewire('resources-by-grade')</div>
+    <div class="col-12">@livewire('monthly-views-chart')</div>
+    <div class="col-12 mb-4">@livewire('resources-by-grade')</div>
   </div>
   
   
